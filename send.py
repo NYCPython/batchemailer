c387d6f76421c86259d5901c8057f9c5fbbb2c70
Paul Logston <code@logston.me>
2015-08-02 23:28:24 -0400

    Paul's take on the batch emailer

--- send.py
+++ send.py
@@ -1,118 +1,104 @@
-import config
-import re
+import argparse
+import configparser
+import csv
+import os
 import smtplib
 
 
-FIELD_DELIMITER = "\t"
-PARAM_PATTERN = "{{([a-z_0-9]*)}}"
-REPLACE_PATTERN = "{{%s}}"
+def main(dry_run=True):
+    if not dry_run:
+        SMTP_PASSWORD = os.environ.get('SMTP_PASSWORD', '').strip()
+        if not SMTP_PASSWORD:
+            print('Must set SMTP_PASSWORD environment variable')
 
+    # Parse and select config
+    config = configparser.ConfigParser()
 
-def remove_junk(text):
-    return re.sub(r"[\x80-\xff]", ' ', text).strip()
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    config_file = os.path.join(cwd, 'batch-emailer.ini')
+    if not os.path.exists(config_file):
+        raise RuntimeError('Please create and populate a batch-emailer.ini file')
 
+    print('Reading configs from {}'.format(config_file))
+    config.read(config_file)
 
-def extract_just_emails(emails):
-    just_emails = []
-    for full_email in emails:
-        just_email = re.compile("<(.*)>").findall(full_email)
-        if just_email:
-            just_emails.append(just_email[0])
-        elif '@' in full_email:
-            just_emails.append(full_email)
-    return just_emails
+    if not config.sections():
+        msg = 'Please create a least one customized section in the batch-emailer.ini file'
+        raise ValueError(msg)
 
-def get_recipients(names, emails):
-    names_list = names.split(',')
-    full_emails = ["%s <%s>" % (names_list[i], x) for i, x in enumerate(emails.split(','))]
-    return ', '.join(full_emails)
+    print('Emails available for sending:')
+    for i, sec in enumerate(config.sections()):
+        print('{}: {}'.format(i, sec))
+    section_index = input('Enter the number of the email you would like to send: ')
+    section_index = int(section_index.strip())
+    section = config.sections()[section_index]
 
+    # Load constants from config
+    SMTP_SERVER = config[section].get('smtp_server')
+    SMTP_EMAIL = config[section].get('smtp_email')
 
-def get_first_names(names):
-    first_names = [x.split(" ")[0] for x in names.split(',')]
-    return ', '.join(first_names)
+    REPLY_ADDRESS = config[section].get('reply_address')
+    CC_ADDRESSES = config[section].get('cc_addresses')
+    if CC_ADDRESSES:
+        CC_ADDRESSES = CC_ADDRESSES.strip().split(',')
+    SUBJECT_TEMPLATE = config[section].get('subject')
+    BODY_FILENAME = config[section].get('body_filename')
+    RECIPIENTS_FILENAME = config[section].get('recipients_filename')
 
+    # Load templates
+    with open(BODY_FILENAME) as f:
+        BODY_TEMPLATE = f.read()
 
-def lookup_key(key, entry_values):
-    if key in entry_values:
-        return entry_values[key]
-    elif key in config.EXTRA_PARAM_KEYS:
-        return config.EXTRA_PARAM_KEYS[key]
-    elif key == 'first_name':
-        return get_first_names(entry_values['email_names'])
+    with open(RECIPIENTS_FILENAME) as f:
+        reader = csv.DictReader(f)
+        RECIPIENT_DICTS = [d for d in reader]
 
+    if not dry_run:
+        # Start server
+        server = smtplib.SMTP(SMTP_SERVER)
+        server.ehlo()
+        server.starttls()
+        server.login(SMTP_EMAIL, SMTP_PASSWORD)
 
-def replace_keys(phrase, entry_values):
-    phrase_keys = re.findall(PARAM_PATTERN, phrase)
-    for key in phrase_keys:
-        param_value = lookup_key(key, entry_values)
-        phrase = re.sub(REPLACE_PATTERN % key, param_value, phrase)
-    return phrase
-
-# ===============================================================
-#   BEGIN SCRIPT
-# ===============================================================
-
-server = smtplib.SMTP(config.SMTP_SERVER)
-server.ehlo()
-server.starttls()
-server.login(config.SMTP_EMAIL, config.SMTP_PWD)
-
-with open(config.FILENAME_BODY, 'r') as f:
-    BODY_TEMPLATE = f.read()
-with open(config.FILENAME_SUBJECT, 'r') as f:
-    SUBJECT_TEMPLATE = f.read().split('\n')[0]
-with open(config.FILENAME_RECIPIENTS_TSV, 'r') as f:
-    mailing_list = f.read().split('\n')
-
-mailing_keys = [x.strip() for x in mailing_list[0].split(FIELD_DELIMITER)]
-
-# a list of key:value dicts for each mailing_list entry
-mailing_recipients = [{mailing_keys[i]:remove_junk(y)
-                       for i, y in enumerate(x.split(FIELD_DELIMITER))}
-                      for x in mailing_list[1:]]
-
-msg = "PREVIEW_MODE is " + str(config.PREVIEW_MODE) + "."
-if not config.PREVIEW_MODE:
-    msg += " **** THIS IS THE REAL THING!!! **** "
-confirm = raw_input(msg + " Enter y to continue: ")
-if confirm != 'y':
-    exit(0)
-
-for entry in mailing_recipients:
-    cc_emails = config.MSG_CC[:] or []
-    if config.HEADER_PARAM_EMAIL in entry:
-        if config.PREVIEW_MODE:
-            entry[config.HEADER_PARAM_EMAIL] = ','.join(config.PREVIEW_EMAILS)
-            entry.pop(config.HEADER_PARAM_CC, None)
-        subject = replace_keys(SUBJECT_TEMPLATE, entry)
-        body = replace_keys(BODY_TEMPLATE, entry)
-        if config.HEADER_PARAM_CC in entry:
-            cc_emails.append(entry[config.HEADER_PARAM_CC])
-
-        headers = ["From: " + config.MSG_FROM,
-                   "To: " + entry[config.HEADER_PARAM_EMAIL],
-                   "Subject: " + subject,
+    # Build and send emails
+    for count, recipient_dict in enumerate(RECIPIENT_DICTS, start=1):
+        subject_line = SUBJECT_TEMPLATE.format(**recipient_dict)
+        headers = ["From: {}".format(REPLY_ADDRESS),
+                   "To: {email_address}".format(**recipient_dict),
+                   "Subject: {}".format(subject_line),
                    "mime-version: 1.0",
                    "content-type: text/plain"]
-
-        if cc_emails:
-            headers.append("CC: " + ', '.join(cc_emails))
-
+        if CC_ADDRESSES:
+            headers.append("CC: " + ', '.join(CC_ADDRESSES))
         headers = "\r\n".join(headers)
+        body = BODY_TEMPLATE.format(**recipient_dict)
+        msg = headers + "\r\n\r\n" + body
 
-        print "============================================="
-        print headers
-        print body[:70], "..."
-        print "============================================="
-        confirm = raw_input("Do you really want to send this to %r? " % entry[config.HEADER_PARAM_EMAIL])
-        if confirm == 'y':
-            #collect all emails in To, From, and CC fields
-            send_to_emails = entry[config.HEADER_PARAM_EMAIL] + ',' + config.MSG_FROM + ',' \
-                + ','.join(cc_emails)
-            send_to_emails = extract_just_emails(send_to_emails.split(','))
-            server.sendmail(config.MSG_FROM,
-                            send_to_emails,
-                            headers + "\r\n\r\n" + body)
+        if dry_run:
+            print(msg, '\n\n')
+        else:
+            print('Sending email #{}: "{}" to <{email_address}>'
+                  ''.format(count, subject_line, **recipient_dict))
+            confirm = input('Confirm [y/n]: ')
+            confirm = confirm.strip()
+            if 'y' not in confirm:
+                continue
+            destination_emails = [recipient_dict['email_address']] + CC_ADDRESSES
+            server.sendmail(REPLY_ADDRESS, destination_emails, msg.encode('utf-8'))
+            print('...sent.')
 
-server.quit()
+    if not dry_run:
+        server.quit()
+    print('Done.')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='A tool to send emails.')
+    parser.add_argument('--nodry',
+                        dest='dry',
+                        action='store_false',
+                        default=True,
+                        help='Send emails. Do not run in dry test mode')
+    args = parser.parse_args()
+    main(dry_run=args.dry)
+
